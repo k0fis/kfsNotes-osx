3bd1bbf56aa3ae4b53cccbf11afcb5e45dc6ec63
kofis <pavel.drimalka@gmail.com>
2025-12-26 00:34:03 +0100

    Initial Commit

---
+++ kfsNotes/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  kfsNotes
+//
+//  Created by Pavel Dřímalka on 26.12.2025.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
