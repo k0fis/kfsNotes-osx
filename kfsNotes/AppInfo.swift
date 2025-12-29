9e708e879d58e2360f0df66f1365d8898f810696
kofis <pavel.drimalka@gmail.com>
2025-12-29 23:39:22 +0100

    Add info dialog

--- kfsNotes/AppInfo.swift
+++ kfsNotes/AppInfo.swift
@@ -5,11 +5,9 @@
 //  Created by Pavel Dřímalka on 29.12.2025.
 //
 
-import Foundation
-
 struct AppInfo {
-    static let version = ProcessInfo.processInfo.environment["APP_VERSION"] ?? "v0.0.0"
-    static let build = ProcessInfo.processInfo.environment["APP_BUILD"] ?? "0"
+    static let version = "v0.0.0"
+    static let build = "0"
 
     static let full: String = "\(version) (\(build))"
 }
