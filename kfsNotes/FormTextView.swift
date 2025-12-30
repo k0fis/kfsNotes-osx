dc9d7c3d6ac27f023268d6813fb77c5af39aaa82
kofis <pavel.drimalka@gmail.com>
2025-12-30 01:09:43 +0100

    formating

--- kfsNotes/FormTextView.swift
+++ kfsNotes/FormTextView.swift
@@ -1,29 +1,48 @@
 import Cocoa
 
 final class FormTextView: NSTextView {
-    
-    override func viewDidMoveToWindow() {
-        super.viewDidMoveToWindow()
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
 
         isRichText = false
         allowsUndo = true
 
-        drawsBackground = false
-        backgroundColor = .clear
+        //  avoid adaptive vibrant background
+        drawsBackground = true
+        backgroundColor = NSColor.textBackgroundColor
 
-        usesAdaptiveColorMappingForDarkAppearance = false
-        textColor = .textColor
+        // non-adaptive always visible text color
+        textColor = NSColor.controlTextColor
         font = NSFont.systemFont(ofSize: NSFont.systemFontSize)
 
         typingAttributes = [
-            .foregroundColor: NSColor.textColor,
+            .foregroundColor: NSColor.controlTextColor,
             .font: font!
         ]
-        print("FormTextView awakeFromNib")
+    }
+
+    override func insertText(_ insertString: Any, replacementRange: NSRange) {
+        let plain: String
+        if let attr = insertString as? NSAttributedString {
+            plain = attr.string
+        } else if let str = insertString as? String {
+            plain = str
+        } else {
+            return
+        }
+        super.insertText(plain, replacementRange: replacementRange)
+    }
+
+    override func paste(_ sender: Any?) {
+        let pb = NSPasteboard.general
+        if let string = pb.string(forType: .string) {
+            self.string = string
+        }
     }
 
     override func keyDown(with event: NSEvent) {
-        if event.keyCode == 48 { // TAB
+        if event.keyCode == 48 {
             if event.modifierFlags.contains(.shift) {
                 window?.selectPreviousKeyView(nil)
             } else {
